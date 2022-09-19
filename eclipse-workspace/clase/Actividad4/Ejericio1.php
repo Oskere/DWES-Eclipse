8544527487f897e6649cbdc4f5fd0395178bda18
2daw3 <2daw3@HZ114489.H012108.NET>
2022-09-19 10:40:29 +0200

    2do commit

--- eclipse-workspace/clase/Actividad4/Ejericio1.php
+++ eclipse-workspace/clase/Actividad4/Ejericio1.php
@@ -6,3 +6,20 @@ $apellido1="mars";
 $apellido2="vazquez";
 $dni1="11111111a";
 $dni2="11111111b";
+$r=0;
+$v=0;
+$array = array("Nombre" => $nombre1,"Nombre2" => $nombre2, "Apellido" => $apellido1,"Apellido2" => $apellido2, "DNI" => $dni1, "DNI2" => $dni2);
+
+$s = '<table border="1">';
+foreach ( $array as $r ) {
+    $s .= '<tr>';
+    foreach ( $r as $v ) {
+        $s .= '<td>'.$v.'</td>';
+    }
+    $s .= '</tr>';
+}
+$s .= '</table>';
+
+echo $s;
+
+?>
