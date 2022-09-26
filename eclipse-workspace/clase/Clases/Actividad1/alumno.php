47ae0556e95ce805bf10f041030e2af692f8d78a
Oscar <ikaol@gmail.com>
2022-09-26 11:45:15 +0200

    funsiona

--- eclipse-workspace/clase/Clases/Actividad1/alumno.php
+++ eclipse-workspace/clase/Clases/Actividad1/alumno.php
@@ -1,1 +1,19 @@
 <?php
+class Alumno {
+    private $CodMatricula = "";
+    private $ciclo = "";
+    
+    public function setAlumno($CodMatricula, $ciclo) {
+        $this->CodMatricula= $CodMatricula;
+        $this->ciclo = $ciclo;
+    }
+    
+    public function get_Cod() {
+        return $this->CodMatricula;
+    }
+    
+    public function get_ciclo() {
+        return $this->ciclo;
+    }
+    
+}
