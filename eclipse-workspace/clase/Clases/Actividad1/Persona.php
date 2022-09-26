310c743d00df1e7f216e0958db782681201abf29
2daw3 <2daw3@HZ114489.H012108.NET>
2022-09-26 10:52:44 +0200

    padreando

--- eclipse-workspace/clase/Clases/Actividad1/Persona.php
+++ eclipse-workspace/clase/Clases/Actividad1/Persona.php
@@ -1,4 +1,50 @@
 <?php
-class Personas{
-    private $name;
+class Persona {
+    private $name = "";
+    private $dni = "";
+    private $emaiol = "";
+    
 }
+
+class Alumno {
+    private $CodMatricula = "";
+    private $ciclo = "";
+    
+    public function setAlumno($CodMatricula, $ciclo) {
+        $this->CodMatricula= $CodMatricula;
+        $this->ciclo = $ciclo;
+    }
+    
+    public function get_Cod() {
+        return $this->CodMatricula;
+    }
+    
+    public function get_ciclo() {
+        return $this->ciclo;
+    }
+    
+}
+
+class Docente {
+    private $sueldo = 0;
+    
+    public function setDocente($sueldo) {
+        $this->sueldo = $sueldo;
+    }
+    
+    public function get_sueldo() {
+        return $this->sueldo;
+    }
+}
+
+class CentroFP {
+    private $cod = "";
+    private $nombre = "";
+    
+    public function setCod($cod) {
+        $this->cod = $cod;
+        
+    }
+    
+}
+﻿
