e63c5b4cad5585db9443dfcae599ce0b6740efdb
2daw3 <2daw3@HZ114489.H012108.NET>
2022-09-26 10:47:54 +0200

    4to commit

---
+++ eclipse-workspace/clase/Actividad5/Ejercicio1.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Hola <?php echo $_POST["nombre"]; ?><br>
+Tu contraseña supersegura y que nadie ve es: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
---
+++ eclipse-workspace/clase/Clases/Actividad1/Persona.php
@@ -0,0 +1,4 @@
+<?php
+class Personas{
+    private $name;
+}
