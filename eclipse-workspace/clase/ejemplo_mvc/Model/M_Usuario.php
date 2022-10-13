539950d60841cf8b68a89608e7fe7149d8d3ac2e
Oscar <ikaol@gmail.com>
2022-10-13 10:26:27 +0200

    mvc

---
+++ eclipse-workspace/clase/ejemplo_mvc/Model/M_Usuario.php
@@ -0,0 +1,29 @@
+<?php
+namespace Model;
+require_once 'Conexion.php';
+require_once 'Model/Usuario.php';
+
+class M_Usuario extends Conexion
+{
+    public function getUsers(){
+        $query = parent::con()->query('SELECT * FROM usuarios');
+        
+        $retorno = [];
+        
+        while($fila = $query->fetch_assoc()){
+            $retorno[] = $fila;
+        }
+        
+        return  $retorno;
+    }
+    
+    public function insertUsuario(Usuario $usuario){
+        $sentencia = parent::con()->prepare("INSERT INTO usuarios(username, password, nombre) VALUES (?,?,?)");
+        
+        $sentencia->bind_param("sss", $usuario->getUsername(), $usuario->getPassword(), $usuario->getNombre());
+        
+        $sentencia->execute();
+        $sentencia->close();
+    }
+}
+
